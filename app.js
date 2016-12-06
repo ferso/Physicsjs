41433a0f838ab2a329f9f7c66c09b498faff3c42
Fernando Soto <erickfernando@gmail.com>
2016-12-06 12:49:50 -0600

    Class files

--- app.js
+++ app.js
@@ -23,32 +23,8 @@
         };
 }());
 
-class Ball {
-  constructor(o) {
-  	var o		 = o || {};
-	this.loop    = null;
-	this.radius  = 20;
-	this.color   = '#0000ff';
-	this.x       = o.x || 0;
-	this.y       = o.y || 0;
-	this.context = o.context || null;
-	
-  }
 
-  draw(){  		
-	this.context.fillStyle = this.color;
-	this.context.beginPath();
-	this.context.arc(this.x,this.y,this.radius,0,2*Math.PI,true);
-	this.context.closePath();
-	this.context.fill();
-  }
-  text(str){  	  
-  	  this.context.fillStyle    = "#000";
-  	  this.context.font = "bold 20px sans";
-	  this.context.textBaseline = "top";
-	  this.context.fillText(str, this.x-this.radius/4 ,this.y-this.radius/2);
-  }
-}
+import Ball form './Class/Ball';
 
 export default class App {
 	constructor() {
